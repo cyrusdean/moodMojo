e1834f1b04e534a607cd859ff031931395d11ee5
Cyrus John Dean <cyrusjdean@gmail.com>
2021-09-20 04:51:06 -0600

    New build

--- src/pages/SpotifyCallback/SpotifyCallback.js
+++ src/pages/SpotifyCallback/SpotifyCallback.js
@@ -1,11 +1,11 @@
 import React, { useEffect } from 'react'
 
-const SpotifyCallback = ({ history }) => {
+const SpotifyCallback = () => {
 	useEffect(() => {
 		localStorage.accessTokenExpiration = Date.now() + 3600 * 1000
 		const [, accessToken] = window.location.hash.split('=')
 		localStorage.accessToken = accessToken
-		history.push('/generator')
+		window.location.pathname = '/generator'
 	}, [])
 
 	return (<div></div>)
