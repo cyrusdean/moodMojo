2be4358c516c4f402f5018d4b62196ac10a11f9f
Jake Madsen <jake@equasion.com>
2022-11-07 15:52:45 -0700

    adjustments

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,6 +1,35 @@
 import React from 'react';
 import { Headphones, Sliders, Records, HeroScene } from './com';
+import Lottie from 'react-lottie';
+import * as moodData from './meditating-man.json';
+import * as sliderData from './sliders.json';
+import * as headphonesData from './eq.json';
 import './Home.scss';
+
+const moodDataOptions = {
+  loop: true,
+  autoplay: true,
+  animationData: moodData,
+  rendererSettings: {
+    preserveAspectRatio: 'xMidYMid slice'
+  }
+};
+const sliderDataOptions = {
+  loop: true,
+  autoplay: true,
+  animationData: sliderData,
+  rendererSettings: {
+    preserveAspectRatio: 'xMidYMid slice'
+  }
+};
+const headphonesDataOptions = {
+  loop: true,
+  autoplay: true,
+  animationData: headphonesData,
+  rendererSettings: {
+    preserveAspectRatio: 'xMidYMid meet'
+  }
+};
 
 const Home = () => {
   return (
@@ -13,10 +42,18 @@ const Home = () => {
             <button>Get started</button>
           </a>
         </div>
-        <Records />
+        <div className="lottieWrapper">
+          <Lottie options={moodDataOptions} />
+        </div>
+
+        {/* <Records /> */}
       </div>
       <div className="step sliders-step">
-        <Sliders />
+        <div className="lottieWrapper">
+          <Lottie options={sliderDataOptions} />
+        </div>
+
+        {/* <Sliders /> */}
         <div>
           <h1>Adjust your mood setting preferences.</h1>
           <a href="/generator">
@@ -31,7 +68,11 @@ const Home = () => {
             <button>Get started</button>
           </a>
         </div>
-        <Headphones />
+        {/* <Headphones />
+         */}
+        <div className="lottieWrapper">
+          <Lottie options={headphonesDataOptions} />
+        </div>
       </div>
     </div>
   );
